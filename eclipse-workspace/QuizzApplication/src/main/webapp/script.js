4f68703b5c19b6e50d81b45de1034933db5ffd8d
Jeffreymedwin <Jeffreymedwin@POINTEL.COM>
2024-04-22 15:04:32 +0530

    project

---
+++ eclipse-workspace/QuizzApplication/src/main/webapp/script.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+ function isChecked(){
+	 var checkBox = document.getElementById("formGroupExampleInput2");
+	 
+	 if(checkBox.type === "password") {
+		 checkBox.type = "text";
+	 } else {
+		 checkBox.type = "password";
+	 }
+ } 
